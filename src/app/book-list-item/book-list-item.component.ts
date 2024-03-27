263e227877c5b458c1e8f82edd4ce1abf1f61e4e
Christian Schellenberger <chris.schell@gmx.de>
2024-03-27 20:23:32 +0100

    add book-list-item component: ng g component book-list-item

---
+++ src/app/book-list-item/book-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-book-list-item',
+  templateUrl: './book-list-item.component.html',
+  styleUrl: './book-list-item.component.css'
+})
+export class BookListItemComponent {
+
+}
