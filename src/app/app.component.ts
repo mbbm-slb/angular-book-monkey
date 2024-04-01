c6b92c83fc83ded8ea4b0e1e382d8366d34f5f1a
Christian Schellenberger <chris.schell@gmx.de>
2024-04-01 08:39:24 +0200

    add logic to AppComponent for managing the selected book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './shared/book';
 
 @Component({
   selector: 'bm-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  book: Book | null = null;
+
+  showList() {
+    this.book = null;
+  }
+
+  showDetails(book: Book) {
+    this.book = book;
+  }
 }
