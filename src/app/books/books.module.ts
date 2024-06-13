58bd155643d459f202eff29e1673aef2d991f18d
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-13 08:54:10 +0200

    move components to new feature module (adapt references accordingly)

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
 
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent
+  ],
   imports: [
     CommonModule,
     BooksRoutingModule
