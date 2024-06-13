f1d7db6062efd086f22c32cd7d35e81dc7b72d7e
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-13 10:32:14 +0200

    cleanup: remove all class content from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { Book } from './shared/book';
 
 @Component({
   selector: 'bm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  book: Book | null = null;
-
-  showList() {
-    this.book = null;
-  }
-
-  showDetails(book: Book) {
-    this.book = book;
-  }
-}
+export class AppComponent {}
