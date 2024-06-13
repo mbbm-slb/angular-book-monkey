2910452273bd31766bfe729ae99d70e4ba3f3619
slb <CSchellenberger@MuellerBBM-vas.de>
2024-06-13 08:56:22 +0200

    export components from feature-module

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -17,6 +17,10 @@ import { BookDetailsComponent } from './book-details/book-details.component';
   imports: [
     CommonModule,
     BooksRoutingModule
+  ],
+  exports: [
+    BookListComponent,
+    BookDetailsComponent
   ]
 })
 export class BooksModule { }
