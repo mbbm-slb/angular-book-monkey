30545bc847dd8d393845d5310b0fd95bd44995bf
slb <CSchellenberger@MuellerBBM-vas.de>
2024-07-31 20:29:04 +0200

    introduce remove book method and button

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { BookStoreService } from '../../shared/book-store.service';
 import { Book } from '../../shared/book';
@@ -14,10 +14,19 @@ export class BookDetailsComponent {
   constructor(
     private service: BookStoreService,
     private route: ActivatedRoute,
+    private router: Router
   ) {
     const isbn = this.route.snapshot.paramMap.get('isbn')!;
     this.service.getSingle(isbn).subscribe(book => {
       this.book = book;
     });
+  }
+
+  removeBook(isbn: string) {
+    if (window.confirm('Remove book?')) {
+      this.service.remove(isbn).subscribe(() => {
+        this.router.navigateByUrl('/books');
+      });
+    }
   }
 }
