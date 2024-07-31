706cb5cfe58361324bbf09cc5611682b313603bd
slb <CSchellenberger@MuellerBBM-vas.de>
2024-07-31 17:51:48 +0200

    change BookStoreService to use API

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Book } from './book';
 
@@ -6,36 +8,21 @@ import { Book } from './book';
   providedIn: 'root'
 })
 export class BookStoreService {
+  private apiURL = 'https://api5.angular-buch.com';
   private books: Book[] = [];
 
-  constructor() {
-    this.books = [
-      {
-        isbn: '12345',
-        title: 'Tierisch gut kochen',
-        authors: ['Mrs Chimp', 'Mr Gorilla'],
-        published: '2022-06-20',
-        subtitle: 'Rezepte von Affe bis Zebra',
-        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
-        description: 'Immer lecker und gut'
-      },
-      {
-        isbn: '67890',
-        title: 'Backen mit Affen',
-        authors: ['Orang Utan'],
-        published: '2022-07-15',
-        subtitle: 'Bananenbrot und mehr',
-        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
-        description: 'Tolle Backtipps für Mensch und Tier'
-      }
-    ];
+  constructor(private http: HttpClient) {
   }
 
-  getAll(): Book[] {
-    return this.books;
+  getAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiURL}/books`);
   }
 
-  getSingle(isbn: string): Book | undefined {
-    return this.books.find(book => book.isbn === isbn);
+  getSingle(isbn: string): Observable<Book> {
+    return this.http.get<Book>(`${this.apiURL}/book/${isbn}`);
+  }
+
+  remove(isbn: string): Observable<unknown> {
+    return this.http.delete(`${this.apiURL}/book/${isbn}`);
   }
 }
