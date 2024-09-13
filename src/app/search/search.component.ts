9797bfceee47ae4919c12e7d61f0e4ff4914c262
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-13 14:42:13 +0200

    add new component search (ng g component search)

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+}
