5076e15974816f1da8d65c1b46eaf85935eaae76
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-13 15:21:39 +0200

    search component: use switchMap operator in combination with BookStoreService to search for books (i.e. send a http-Request  containing the query term from the search input to the http book server)

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, Subject, switchMap } from 'rxjs';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-search',
@@ -9,11 +10,12 @@ import { debounceTime, distinctUntilChanged, filter, Subject } from 'rxjs';
 export class SearchComponent {
   input$ = new Subject<string>();
 
-  constructor() {
+  constructor(private service: BookStoreService) {
     this.input$.pipe(
       filter(term => term.length >= 3),
       debounceTime(500),
-      distinctUntilChanged()
+      distinctUntilChanged(),
+      switchMap(term => this.service.getAllSearch(term))
     )
     .subscribe(console.log);
   }
