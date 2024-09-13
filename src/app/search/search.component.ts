dc347fc22dd2bcf9983c365b31ce40c3d599e6d5
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-13 15:37:12 +0200

    search component: present search results and show/hide busy indicator

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, Subject, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, Observable, Subject, switchMap, tap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'bm-search',
@@ -10,16 +11,16 @@ import { BookStoreService } from '../shared/book-store.service';
 export class SearchComponent {
   input$ = new Subject<string>();
   isLoading = false;
+  results$: Observable<Book[]>
 
   constructor(private service: BookStoreService) {
-    this.input$.pipe(
+    this.results$ = this.input$.pipe(
       filter(term => term.length >= 3),
       debounceTime(500),
       distinctUntilChanged(),
       tap(() => this.isLoading = true),
       switchMap(term => this.service.getAllSearch(term)),
       tap(() => this.isLoading = false)
-    )
-    .subscribe(console.log);
+    );
   }
 }
