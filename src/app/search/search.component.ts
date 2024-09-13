23d1589a37a504df5027f8ed9f382232ef8e5590
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-13 15:27:12 +0200

    sear component: preparing for busy indicator while executing a http request => introducin a flag "isLoading" which is toggeled by the tap-operator in the observable pipeline

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, Subject, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, Subject, switchMap, tap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -9,13 +9,16 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class SearchComponent {
   input$ = new Subject<string>();
+  isLoading = false;
 
   constructor(private service: BookStoreService) {
     this.input$.pipe(
       filter(term => term.length >= 3),
       debounceTime(500),
       distinctUntilChanged(),
-      switchMap(term => this.service.getAllSearch(term))
+      tap(() => this.isLoading = true),
+      switchMap(term => this.service.getAllSearch(term)),
+      tap(() => this.isLoading = false)
     )
     .subscribe(console.log);
   }
