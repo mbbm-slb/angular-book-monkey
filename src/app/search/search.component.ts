b8abe6ef143ab29bc042b8b26c135006ff148606
slb <CSchellenberger@MuellerBBM-vas.de>
2024-09-13 14:57:17 +0200

    add Subject<string>  to search component and a search input field to its template. Subscribe with output to console to test if it is working => should produce a console output at every keystroke.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'bm-search',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  input$ = new Subject<string>();
 
+  constructor() {
+    this.input$.subscribe(console.log); //the book says "this.input$.subscribe(e => console.log(e))" but it's the same thing only shorter
+  }
 }
