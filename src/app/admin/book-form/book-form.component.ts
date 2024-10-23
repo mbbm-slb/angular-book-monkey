e1b8bbba76cc521acd57647ed2f470a5aa27fbdc
slb <CSchellenberger@MuellerBBM-vas.de>
2024-10-23 16:11:47 +0200

    Formularmodell definieren (book-form.component.ts)

--- src/app/admin/book-form/book-form.component.ts
+++ src/app/admin/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { Component,  Output, EventEmitter } from '@angular/core';
 import { Book } from '../../shared/book';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bm-book-form',
@@ -7,16 +8,33 @@ import { Book } from '../../shared/book';
   styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
-  book: Book = {
-    isbn: '',
-    title: '',
-    authors: ['']
-    }
-
     @Output() submitBook = new EventEmitter<Book>();
 
+    form = new FormGroup({
+      title: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      }),
+      subtitle: new FormControl('', { nonNullable: true }),
+      isbn: new FormControl('', {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(13),
+        ]
+      }),
+      description: new FormControl('', { nonNullable: true }),
+      published: new FormControl('', { nonNullable: true }),
+      thumbnailUrl: new FormControl('', { nonNullable: true }),
+    });
+
     submitForm() {
-      this.submitBook.emit(this.book);
+      const formValue = this.form.getRawValue();
+      const newBook: Book = {
+        ...formValue,
+        authors: [], //TODO: echte Eingaben
       }
-      
+      this.submitBook.emit(newBook);
+    }
 }
