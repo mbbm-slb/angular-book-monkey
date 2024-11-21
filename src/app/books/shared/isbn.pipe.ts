98f70dc17a8528133e376dafa962de70795c76c9
wks <steffi.winkler@muellerbbm-vas.com>
2024-11-21 13:31:36 +0100

    Implement isbn pipe

--- src/app/books/shared/isbn.pipe.ts
+++ src/app/books/shared/isbn.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IsbnPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    if (!value) {return '';}
+    return `${value.substring(0, 3)}-${value.substring(3)}`;
   }
 
 }
