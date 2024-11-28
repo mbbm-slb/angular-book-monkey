6e2d3fe64a2666abc6f49a909baae20dba1ef252
slb <Christian.Schellenberger@mbbm-vas.com>
2024-11-28 12:27:20 +0100

    confirm directive: add input property to provide the confirm display text

--- src/app/books/shared/confirm.directive.ts
+++ src/app/books/shared/confirm.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 
 @Directive({
   selector: '[bmConfirm]'
 })
 export class ConfirmDirective {
-
+  @Input ('bmConfirm') confirmText?: string;
   constructor() { }
 
 }
