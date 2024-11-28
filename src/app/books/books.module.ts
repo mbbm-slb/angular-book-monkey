952a771265b1ffb3eb0fb5c959ee0a9275f1a487
slb <Christian.Schellenberger@mbbm-vas.com>
2024-11-28 12:22:03 +0100

    add "confirm" directive (ng g directive books/shared/confirm)

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -7,6 +7,7 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { IsbnPipe } from './shared/isbn.pipe';
+import { ConfirmDirective } from './shared/confirm.directive';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { IsbnPipe } from './shared/isbn.pipe';
     BookListComponent,
     BookListItemComponent,
     BookDetailsComponent,
-    IsbnPipe
+    IsbnPipe,
+    ConfirmDirective
   ],
   imports: [
     CommonModule,
