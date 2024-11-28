2da987de00ecb401bf45bc5761a42726580a1663
slb <Christian.Schellenberger@mbbm-vas.com>
2024-11-28 13:23:18 +0100

    LoggedinOnlyDirective: subscribe to AuthService on construction, unsubscribe on destruction

--- src/app/books/shared/loggedin-only.directive.ts
+++ src/app/books/shared/loggedin-only.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+
+import { AuthService } from '../../shared/auth.service'
 
 @Directive({
   selector: '[bmLoggedinOnly]'
 })
-export class LoggedinOnlyDirective {
+export class LoggedinOnlyDirective implements OnDestroy {
+  private destroy$ = new Subject<void>();
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.authService.isAuthenticated$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(isAuthenticated => {
+      //TODO
+    });
+   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
 }
