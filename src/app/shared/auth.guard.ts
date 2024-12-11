d2b9892853208cc63f70bb51094cf61f665ee349
wks <steffi.winkler@muellerbbm-vas.com>
2024-12-11 12:23:44 +0100

    inject AuthService and Router

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,5 +1,9 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+    return true;
 };
