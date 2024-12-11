807c9b8a77222a2a739dfce20ed78e22581a564e
wks <steffi.winkler@muellerbbm-vas.com>
2024-12-11 12:49:19 +0100

    change guard to return an Observable

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,16 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { map, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated) {
-    return true;
-  }
-  else {
-    window.alert('Not logged in!');
-    return router.parseUrl('/home');
-  }
+  return authService.isAuthenticated$.pipe(
+    take(1),
+    map(isAuthenticated => {
+      if (isAuthenticated) {
+        return true;
+      }
+      else {
+        window.alert('Not logged in!');
+        return router.parseUrl('/home');
+      }
+      })
+    )
 };
