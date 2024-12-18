9b128d81706bebf02c9bae5fd22d0b5a76a1121d
slb <CSchellenberger@MuellerBBM-vas.de>
2024-12-18 15:09:36 +0100

    refactor to standalone - books pipes and directives: import isbn.pipe, confirm.directive and loggedin-only.directive to books.module.ts since as of now they are no longer part of this module.

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -6,23 +6,23 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { IsbnPipe } from './shared/isbn.pipe';
-import { ConfirmDirective } from './shared/confirm.directive';
-import { LoggedinOnlyDirective } from './shared/loggedin-only.directive';
+import { IsbnPipe } from '../shared/isbn.pipe';
+import { ConfirmDirective } from '../shared/confirm.directive';
+import { LoggedinOnlyDirective } from '../shared/loggedin-only.directive';
 
 
 @NgModule({
   declarations: [
     BookListComponent,
     BookListItemComponent,
-    BookDetailsComponent,
-    IsbnPipe,
-    ConfirmDirective,
-    LoggedinOnlyDirective
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    IsbnPipe,
+    ConfirmDirective,
+    LoggedinOnlyDirective
   ]
 })
 export class BooksModule { }
