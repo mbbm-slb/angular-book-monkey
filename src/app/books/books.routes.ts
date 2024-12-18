629703567da7a7917ca23d8eabf568a2b05d4dc4
slb <CSchellenberger@MuellerBBM-vas.de>
2024-12-18 15:50:01 +0100

    refactor to standalone - books module: provide routing for feature "books" with lazy loading

--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [
+export const BOOKS_ROUTES: Routes = [
   {
     path: '',
     component: BookListComponent,
@@ -14,9 +13,3 @@ const routes: Routes = [
     component: BookDetailsComponent,
   }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class BooksRoutingModule { }
