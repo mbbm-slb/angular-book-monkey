420538bd8c2bff5a6e10b996612df23bc48266d8
slb <CSchellenberger@MuellerBBM-vas.de>
2024-12-18 15:33:49 +0100

    refactor to standalone - books module: make books.* components standalone

--- src/app/books/book-list-item/book-list-item.component.ts
+++ src/app/books/book-list-item/book-list-item.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { NgFor,NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { Book } from '../../shared/book';
+import { IsbnPipe } from '../../shared/isbn.pipe';
 
 @Component({
   selector: 'bm-book-list-item',
   templateUrl: './book-list-item.component.html',
-  styleUrl: './book-list-item.component.css'
+  styleUrl: './book-list-item.component.css',
+  standalone: true,
+  imports: [
+    NgIf, NgFor, RouterLink, 
+    IsbnPipe
+  ]
 })
 export class BookListItemComponent {
   @Input() book?: Book;
