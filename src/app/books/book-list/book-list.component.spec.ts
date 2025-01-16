5fe34d16a93531cc6ebc1c1960dc82c7fe140b4c
slb <CSchellenberger@MuellerBBM-vas.de>
2025-01-16 10:15:01 +0100

    Isolierte Unit-Tests: Komponenten testen * Was – Wir beweisen, dass die simple BookListComponent beim Aufruf der Methode showDetails() das übergebene Buch per Event weiterreicht. * Wie – Isolierter Unit-Test * Warum – Die Komponente kann direkt instanziiert werden, da wir keine Dinge prüfen werden, die eine gerenderte View benötigen. Der Einsatz von TestBed ist bei dieser Einschränkung nicht notwendig.

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -2,9 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
 
-describe('BookListComponent Dummy', () => {
-  it('should pass this dummy test', () => {
-    const value = true;
-    expect(value).toBeTrue();
+import { Book } from '../../shared/book';
+
+describe('BookListComponent', () => {
+  let component: BookListComponent;
+
+  beforeEach(() => {
+    component = new BookListComponent();
+  });
+
+  // trivial test
+  it('should hold a hardcoded list of 2 books', () => {
+    expect(component.books).toHaveSize(2);
+  });
+
+  // test subscribes to the event emitter and checks if the book is passed correctly as event payload
+  it('should trigger an event on "doSelect"', () => {
+    const sentBook = {} as Book;
+    let receivedBook: Book | undefined;
+    component.selectBook.subscribe(book => {
+      receivedBook = book;
+    });
+    component.doSelect(sentBook);
+    expect(receivedBook).toBe(sentBook);  // "toBe" compares references! i.e. variable must be the same object
   });
 });
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -4,15 +4,41 @@ import { BookStoreService } from '../../shared/book-store.service';
 import { Book } from '../../shared/book';
 import { Observable } from 'rxjs';
 
+import { EventEmitter, Output } from '@angular/core';
+
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent {
-  books$: Observable<Book[]>;
+  books: Book[] = [];
+  @Output() selectBook = new EventEmitter<Book>();
 
-  constructor(private service: BookStoreService) {
-    this.books$ = this.service.getAll();
+  constructor() {
+    this.books = [
+      {
+        isbn: '12345',
+        title: 'Tierisch gut kochen',
+        authors: ['Mrs Chimp', 'Mr Gorilla'],
+        published: '2022-06-20',
+        subtitle: 'Rezepte von Affe bis Zebra',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Immer lecker und gut'
+      },
+      {
+        isbn: '67890',
+        title: 'Backen mit Affen',
+        authors: ['Orang Utan'],
+        published: '2022-07-15',
+        subtitle: 'Bananenbrot und mehr',
+        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
+        description: 'Tolle Backtipps für Mensch und Tier'
+      }
+    ];
+  }
+
+  doSelect(book: Book) {
+    this.selectBook.emit(book);
   }
 }
