cb9bace2b4bb0b4b6bcca886c58533fc99f8285d
slb <CSchellenberger@MuellerBBM-vas.de>
2025-01-16 13:28:28 +0100

    Shallow Component Test: einzelne Komponenten testen (Fixed with "shallow component strategy") * Was – Wir beweisen, dass die simple BookListComponent bei einem Klick auf die Kindkomponente ein Buch per Event weiterreicht. * Wie – Shallow Component Test (mit TestBed) * Warum – Wir benötigen eine gerenderte View, um die Interaktion mit dem DOM zu prüfen. Der Einsatz von TestBed ist hierfür notwendig!

--- src/app/books/book-list/book-list.component.spec.ts
+++ src/app/books/book-list/book-list.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookListComponent } from './book-list.component';
 import { Book } from '../../shared/book';
 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BookListComponent]
+      declarations: [BookListComponent],
+      schemas: [NO_ERRORS_SCHEMA] // Shallow Unit Test. BookListItem wird nicht gerendert. Alternativ muss man BookListItem hier einbinden. Siehe Kap. 8
     }).compileComponents();
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
