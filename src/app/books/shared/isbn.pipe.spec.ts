841c5d6b8bc63469177d7c6d0e68a57edd8e394f
slb <CSchellenberger@MuellerBBM-vas.de>
2025-01-16 09:07:02 +0100

    Isolierte Unit-Tests: Pipes testen * Was – Wir beweisen, dass die IsbnPipe eine ISBN mit einem Bindestrich trennt und dass unpassende Werte ignoriert werden. * Wie – Isolierter Unit-Test * Warum – Die Pipe kann direkt instanziiertwerden, der Einsatz von TestBed ist nicht notwendig.

--- src/app/books/shared/isbn.pipe.spec.ts
+++ src/app/books/shared/isbn.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { IsbnPipe } from './isbn.pipe';
 
 describe('IsbnPipe', () => {
+  let pipe: IsbnPipe;
+
+  beforeEach(() => {
+    pipe = new IsbnPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new IsbnPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should format an ISBN with a dash', () => {
+    expect(pipe.transform('9783864909467'))
+    .toBe('978-3864909467');
+    });
+    it('should ignore empty values', () => {
+    expect(pipe.transform('')).toBe('');
+    });
 });
