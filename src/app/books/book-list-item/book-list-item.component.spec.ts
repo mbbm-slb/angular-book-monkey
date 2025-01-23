253dd960e738ae9d142a823c3c31293a3532a856
slb <CSchellenberger@MuellerBBM-vas.de>
2025-01-23 08:48:41 +0100

    Komponenten mit Routen testen - Was – Wir beweisen, dass die BookListItemComponent bei einem Klick zueiner neuen Adresse navigiert. - Wie – Unit-Test mit RouterTestingModule unter Verwendung von TestBed - Warum – Das Template der BookListItemComponent verwendet den routerLink. Wir stellen Ersatz per RouterTestingModule zur Verfügung. Der Einsatz von TestBed ist hierfür notwendig.

--- src/app/books/book-list-item/book-list-item.component.spec.ts
+++ src/app/books/book-list-item/book-list-item.component.spec.ts
@@ -1,10 +1,39 @@
+import { Location } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BookListItemComponent } from './book-list-item.component';
+import { IsbnPipe } from '../shared/isbn.pipe';
 
-describe('BookListItemComponent Dummy', () => {
-  it('should pass this dummy test', () => {
-    const value = true;
-    expect(value).toBeTrue();
+describe('BookListItemComponent', () => {
+  let component: BookListItemComponent;
+  let fixture: ComponentFixture<BookListItemComponent>;
+  let location: Location;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BookListItemComponent, IsbnPipe],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: ':isbn', children: [] }
+        ])
+      ]
+    }).compileComponents();
+    location = TestBed.inject(Location);
+    fixture = TestBed.createComponent(BookListItemComponent);
+    component = fixture.componentInstance;
+    // Buch zum Anzeigen
+    component.book = {
+      isbn: '111',
+      title: 'Book 1',
+      authors: [],
+      thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png'
+    };
+    fixture.detectChanges();
+  });
+
+  it('should navigate to details page by ISBN', async () => {
+    fixture.nativeElement.querySelector('img').click();
+    await fixture.whenStable();
+    expect(location.path()).toEqual('/111');
   });
 });
