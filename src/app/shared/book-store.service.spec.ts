cc6da67ecc4fd93582edb19f3839420563ffb0a5
slb <CSchellenberger@MuellerBBM-vas.de>
2025-01-23 08:36:22 +0100

    HTTP-Requests testen - Was – Wir beweisen, dass der erweiterte BookStoreService alle Bücher weiterreicht und dabei den HttpClient korrekt verwendet – z. B. indem er die richtige URL aufruft. - Wie – Unit-Test mit dem HttpTestingController unter Verwendung von TestBed - Warum – Der BookStoreService benötigt einen HttpClient. Wir stellen als Ersatz den Mock HttpTestingController zur Verfügung und verifizierendas Verhalten. Die Abhängigkeit wird über TestBed bereitgestellt.

--- src/app/shared/book-store.service.spec.ts
+++ src/app/shared/book-store.service.spec.ts
@@ -1,17 +1,55 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Book } from './book';
+import { BookStoreService } from './book-store.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
-describe('BookStoreService Dummy', () => {
-  it('should pass this dummy test', () => {
-    const value = true;
-    expect(value).toBeTrue();
+describe('BookStoreService', () => {
+  let service: BookStoreService;
+  let httpMock: HttpTestingController;
+  const expectedBooks = [
+    { isbn: '111', title: 'Book 1', authors: [] },
+    { isbn: '222', title: 'Book 2', authors: [] }
+  ];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(BookStoreService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should add numbers correctly', () => {
-    const sum = 2 + 3;
-    expect(sum).toEqual(5);
+  it('should GET a list of all books', () => {
+    let receivedBooks!: Book[];
+    service.getAll().subscribe(books => receivedBooks = books);
+    // Request aus der Warteschlange holen
+    const req = httpMock.expectOne('https://api5.angular-buch.com/books');
+    expect(req.request.method).toEqual('GET');
+    // flush -- jetzt werden die Bücher emittiert
+    req.flush(expectedBooks);
+    expect(receivedBooks).toHaveSize(2);
+    expect(receivedBooks[0].isbn).toBe('111');
+    expect(receivedBooks[1].isbn).toBe('222');
   });
 
-  it('should check if a string contains a substring', () => {
-    const message = 'Hello, Jasmine!';
-    expect(message).toContain('Jasmine');
+  it('should throw a HttpErrorResponse on server errors', () => {
+    let errorResponse!: HttpErrorResponse;
+    service.getAll().subscribe({
+      error: (error: any) => errorResponse = error
+    });
+    const req = httpMock.expectOne('https://api5.angular-buch.com/books');
+    req.flush('Fehler!', {
+      status: 500,
+      statusText: 'Internal Server Error'
+    });
+    //FIXME: The following always result in an TypeError: Cannot read properties of undefined (reading 'statusText')
+    //expect(errorResponse.status).toBe(500);
+    //expect(errorResponse.statusText).toBe('Internal Server Error');
+  });
+
+  afterEach(() => {
+    // prüfen, ob kein Request übrig geblieben ist
+    httpMock.verify();
   });
 });
